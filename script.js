16544db0cc85f1f18be6e04b81610c9f50814381
pipiolus <giovanipaolonii@gmail.com>
2023-04-24 15:36:03 -0300

    A few more changes on UI

--- script.js
+++ script.js
@@ -1,77 +1,31 @@
-let playerWins = 0;
-let computerWins = 0;
+let playerScore = 0;
+let pcScore = 0;
 
-function getComputerChoice() {
-    
+let weapons = document.querySelectorAll('.btn');
+
+
+const getComputerChoice = () => {
+
     let choices = ['rock', 'paper', 'scissors'];
 
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    let pcChoice = choices[Math.floor(Math.random() * choices.length)];
 
-   return computerChoice;
-
+    return pcChoice;
 }
 
-function playRound (playerPick, computerPick) {
+const playGame = () => {
+    let playerChoice;
 
-    computerPick = getComputerChoice();
-
-    playerPick = prompt("Choose your weapon!").toLowerCase();
-   
-
-    //round results
-
-    if (computerPick === playerPick) {
-
-        return "this round was a tie!";
-    }
-
-    if (computerPick === 'rock' && playerPick === 'paper') {
-
-        playerWins += 1;
-        return ("You win! Paper beats Rock!");
-    }
-    
-    if (computerPick === 'rock' && playerPick === 'scissors') {
-
-        computerWins += 1;
-        return "You loose! Rock beats Scissors!";
-    }
-
-    if (computerPick === 'paper' && playerPick === 'rock') {
-
-        computerWins += 1;
-        return "You loose! Paper beats Rock!";
-
-    }
-
-    if (computerPick === 'paper' && playerPick === 'scissors') {
-
-        playerWins += 1;
-        return "You Win! Scissors beats Paper!";
-    }
-
-    if (computerPick === 'scissors' && playerPick === 'rock') {
-        playerWins += 1;
-        return "You win! Rock beats Scissors!";
-    }
-
-    if (computerPick === 'scissors' && playerPick === 'paper') {
-
-        computerWins += 1;
-        return "You loose! Scissors beats Paper!";
-    }
+    weapons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.classList.contains('rock')) {
+                playerChoice = 'rock';
+                console.log(playerChoice)
+            } else if (button.classList.contains('paper')) {
+                playerChoice = 'paper';
+            } else {
+                playerChoice = 'scissors';
+            }
+        });
+    });
 }
-
-function game(){
-    if (computerWins === 5) {
-        return "Computer won this game!"
-    }
-    
-    else if (playerWins === 5) {
-        return "You won this game!"
-    }
-
-    else {
-       return playRound();
-    }
-};
